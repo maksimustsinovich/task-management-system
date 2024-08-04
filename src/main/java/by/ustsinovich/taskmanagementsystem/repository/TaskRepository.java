3d0c4a69b12fa87fcce1c93fff33cd6517b0f8cc
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-08-04 11:55:53 +0300

    feat: add Task and User repositories

--- src/main/java/by/ustsinovich/taskmanagementsystem/repository/TaskRepository.java
+++ src/main/java/by/ustsinovich/taskmanagementsystem/repository/TaskRepository.java
@@ -1,10 +1,15 @@
 package by.ustsinovich.taskmanagementsystem.repository;
 
 import by.ustsinovich.taskmanagementsystem.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Page<Task> findAll(Specification<Task> specification, Pageable pageable);
 
 }
--- src/main/java/by/ustsinovich/taskmanagementsystem/repository/UserRepository.java
+++ src/main/java/by/ustsinovich/taskmanagementsystem/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package by.ustsinovich.taskmanagementsystem.repository;
 
 import by.ustsinovich.taskmanagementsystem.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Page<User> findAll(Specification<User> specification, Pageable pageable);
 
 }
