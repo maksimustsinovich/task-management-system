d8e61dffdda3cb8bafc02d43f5f7fa0693e3dcf8
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-08-07 11:30:57 +0300

    fix: optimize imports

--- src/main/java/by/ustsinovich/taskmanagementsystem/dto/UserDto.java
+++ src/main/java/by/ustsinovich/taskmanagementsystem/dto/UserDto.java
@@ -1,16 +1,15 @@
 package by.ustsinovich.taskmanagementsystem.dto;
 
-import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
